d538d375be84968b9f39c08e765a80339818f0e0
Richard Hanks <richardhanksjr@gmail.com>
2019-06-09 15:24:03 -0400

    Working audio on keys

--- js/PianoKey.js
+++ js/PianoKey.js
@@ -1,18 +1,44 @@
+// import {Howl, Howler} from 'howler';
+// import {Howl, Howler} from "howler";
+
+
 console.log("ready");
 document.addEventListener("ready", ()=> console.log("loaded"));
+let sound = null;
+let playID = null;
 
 $(".key").hover(
+
     function(){
-        console.log("here");
         // $(this).addClass('red');
         $(this).addClass('press');
         $(this).find("div").addClass("visible");
         $(this).find("div").removeClass('hidden');
+        // The note name for the audio file is stored as the id
+        let noteName = $(this).find("div")[0].id;
+        sound = new Howl({
+            src: [`../audio/${noteName}.wav`]
+        });
+        playID = sound.play();
 
     },
     function(){
         $(this).removeClass('red');
         $(this).find("div").addClass('hidden');
         $(this).removeClass("press");
+        if (sound !== null){
+            sound.fade(1, 0, 100, playID);
+        }
     }
 );
+
+
+$(".key").click(
+    function(){
+        console.log("clicked");
+        const sound = new Howl({
+            src: ['../audio/piano_middle_c.mp3']
+        });
+        sound.play();
+    }
+)
